15aed2300801ffa5bd5066eeef4c72673690fd96
deepdev <ainur.khannanov@gmail.com>
2020-05-18 12:15:38 +0300

    interface added

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Первая книга"),
+            new Book(2, "Вторая книга"),
+            new Book(3, "Третья книга")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
